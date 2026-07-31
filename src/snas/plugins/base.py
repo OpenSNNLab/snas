0d471b3ba90c712c630d76abcad850a0aeaa5818
ibrahim <ibrahim.e.sayed@outlook.com>
2026-07-31 13:18:23 +0300

    add  to standardize the plugins output

--- src/snas/plugins/base.py
+++ src/snas/plugins/base.py
@@ -1,6 +1,23 @@
-from typing import Any
+import json
+from abc import ABC
+from dataclasses import asdict, dataclass
 
 import torch.nn as nn
+
+
+@dataclass
+class BaseResult(ABC):
+    execution_count: int = 0
+
+    def to_dict(self) -> dict:
+        return asdict(self)
+
+    def to_json(self, filepath: str) -> None:
+        with open(filepath, "w") as f:
+            json.dump(self.to_dict(), f, indent=4)
+
+    def to_console(self) -> str:
+        return f"Execs: {self.execution_count}"
 
 
 class BasePlugin(nn.Module):
@@ -9,9 +26,9 @@ class BasePlugin(nn.Module):
         self._result = None
 
     @property
-    def result(self) -> Any:
-        if not hasattr(self, "_result") or self._result is None:
-            raise RuntimeError(f"Result not yet captured by {self.__class__.__name__}.")
+    def result(self) -> BaseResult:
+        if getattr(self, "_result", None) is None:
+            raise RuntimeError(f"Data not yet captured by {self.__class__.__name__}.")
         return self._result
 
     def clear(self) -> None:
